264500e35e0df226f60ce14eb0da9ec047daf3ec
livan <livan.afryanto@gmail.com>
2026-03-01 04:30:36 +0700

    targetting net8_0

--- sqlCommandKu.cs
+++ sqlCommandKu.cs
@@ -5,167 +5,166 @@ using System.Data;
 using System.Data.Common;
 using System.Linq;
 
-namespace JomiunsCom
+namespace JomiunsCom;
+
+public partial class sqlCommandKu : IDisposable
 {
-    public partial class sqlCommandKu : IDisposable
+    private readonly DbCommand _cmdOleDBcommand;
+    private readonly sqlConnectionKu _parentSqlConn;
+
+    internal sqlCommandKu(string instrSPName, sqlConnectionKu insqlParentSQLconn)
     {
-        private readonly DbCommand _cmdOleDBcommand;
-        private readonly sqlConnectionKu _parentSqlConn;
+        clearLastCommand();
 
-        internal sqlCommandKu(string instrSPName, sqlConnectionKu insqlParentSQLconn)
+        _parentSqlConn = insqlParentSQLconn;
+        _cmdOleDBcommand = _parentSqlConn.theSQLconn.CreateCommand();
+        _cmdOleDBcommand.CommandText = instrSPName;
+        if (insqlParentSQLconn.databaseType != enDatabaseType.SqLite)
         {
-            clearLastCommand();
-
-            _parentSqlConn = insqlParentSQLconn;
-            _cmdOleDBcommand = _parentSqlConn.theSQLconn.CreateCommand();
-            _cmdOleDBcommand.CommandText = instrSPName;
-            if (insqlParentSQLconn.databaseType != enDatabaseType.SqLite)
-            {
-                _cmdOleDBcommand.CommandType = System.Data.CommandType.StoredProcedure; //defaultnya adalah command SP
-            }
+            _cmdOleDBcommand.CommandType = System.Data.CommandType.StoredProcedure; //defaultnya adalah command SP
         }
+    }
 
-        public sqlCommandKu setCommandTypeAsText()
+    public sqlCommandKu setCommandTypeAsText()
+    {
+        _cmdOleDBcommand.CommandType = System.Data.CommandType.Text;
+        return this;
+    }
+
+    private void doClose()
+    {
+        if (_parentSqlConn.theSQLconn.State == ConnectionState.Open)
         {
-            _cmdOleDBcommand.CommandType = System.Data.CommandType.Text;
+            _parentSqlConn.theSQLconn.Close();
+        }
+    }
+
+    private void doOpen()
+    {
+        if (_parentSqlConn.theSQLconn.State != System.Data.ConnectionState.Open)
+        {
+            _parentSqlConn.theSQLconn.Open();
+        }
+    }
+
+    public DataSet getDataSet()
+    {
+        doOpen();
+        var dtadptDataAdapter = new Microsoft.Data.SqlClient.SqlDataAdapter(_cmdOleDBcommand as Microsoft.Data.SqlClient.SqlCommand);
+        var dsReturnValue = new DataSet();
+        _ = dtadptDataAdapter.Fill(dsReturnValue);
+        doClose();
+        return dsReturnValue;
+    }
+
+    public sqlCommandKu addParams(params IDbDataParameter[] parameter)
+    {
+        if (parameter == null)
             return this;
-        }
+        _cmdOleDBcommand.Parameters.AddRange(parameter);
+        return this;
+    }
 
-        private void doClose()
+    public sqlCommandKu addParamsFromObject(object parameterAndValues)
+    {
+        var aType = parameterAndValues.GetType();
+        var properties = aType.GetProperties();
+
+        foreach (var oo in properties)
         {
-            if (_parentSqlConn.theSQLconn.State == ConnectionState.Open)
-            {
-                _parentSqlConn.theSQLconn.Close();
-            }
-        }
-
-        private void doOpen()
-        {
-            if (_parentSqlConn.theSQLconn.State != System.Data.ConnectionState.Open)
-            {
-                _parentSqlConn.theSQLconn.Open();
-            }
-        }
-
-        public DataSet getDataSet()
-        {
-            doOpen();
-            var dtadptDataAdapter = new Microsoft.Data.SqlClient.SqlDataAdapter(_cmdOleDBcommand as Microsoft.Data.SqlClient.SqlCommand);
-            var dsReturnValue = new DataSet();
-            _ = dtadptDataAdapter.Fill(dsReturnValue);
-            doClose();
-            return dsReturnValue;
-        }
-
-        public sqlCommandKu addParams(params IDbDataParameter[] parameter)
-        {
-            if (parameter == null)
-                return this;
-            _cmdOleDBcommand.Parameters.AddRange(parameter);
-            return this;
-        }
-
-        public sqlCommandKu addParamsFromObject(object parameterAndValues)
-        {
-            var aType = parameterAndValues.GetType();
-            var properties = aType.GetProperties();
-
-            foreach (var oo in properties)
-            {
-                if (_parentSqlConn.databaseType == enDatabaseType.SQLServer)
-                {
-                    var aValue = oo.GetValue(parameterAndValues, null) ?? DBNull.Value;
-                    _cmdOleDBcommand.Parameters.Add(new SqlParameter($"@{oo.Name}", aValue));
-                }
-
-                if (_parentSqlConn.databaseType == enDatabaseType.SqLite)
-                {
-                    var aValue = oo.GetValue(parameterAndValues, null) ?? DBNull.Value;
-                    _cmdOleDBcommand.Parameters.Add(new SqliteParameter($"@{oo.Name}", aValue));
-                }
-            }
-
-            return this;
-        }
-
-        public sqlCommandKu addParams(params (string paramName, object paramValue)[] parameters)
-        {
-            System.Collections.Generic.IEnumerable<DbParameter> aNewProjection =
-                parameters.Select((things) =>
-               {
-                   return _parentSqlConn.databaseType == enDatabaseType.SQLServer
-                       ? new SqlParameter(things.paramName, things.paramValue)
-                       : _parentSqlConn.databaseType == enDatabaseType.SqLite
-                       ? new Microsoft.Data.Sqlite.SqliteParameter(things.paramName, things.paramValue)
-                       : (DbParameter)null;
-               });
-
-            _cmdOleDBcommand.Parameters.AddRange(aNewProjection.ToArray());
-            return this;
-        }
-
-        public sqlCommandKu addParamWithValue(string instrParamName, object inoValue, Action<DbParameter> SqlParamCreatedCallBack)
-        {
-            DbParameter odprmReturnValue = null;
             if (_parentSqlConn.databaseType == enDatabaseType.SQLServer)
             {
-                odprmReturnValue = new Microsoft.Data.SqlClient.SqlParameter(instrParamName, inoValue);
+                var aValue = oo.GetValue(parameterAndValues, null) ?? DBNull.Value;
+                _cmdOleDBcommand.Parameters.Add(new SqlParameter($"@{oo.Name}", aValue));
             }
 
             if (_parentSqlConn.databaseType == enDatabaseType.SqLite)
             {
-                odprmReturnValue = new Microsoft.Data.Sqlite.SqliteParameter(instrParamName, inoValue);
+                var aValue = oo.GetValue(parameterAndValues, null) ?? DBNull.Value;
+                _cmdOleDBcommand.Parameters.Add(new SqliteParameter($"@{oo.Name}", aValue));
             }
-
-            this.addParams(odprmReturnValue);
-            SqlParamCreatedCallBack?.Invoke(odprmReturnValue);
-            return this;
         }
 
-        public sqlCommandKu addParamWithValue(string instrParamName, object inoValue)
+        return this;
+    }
+
+    public sqlCommandKu addParams(params (string paramName, object paramValue)[] parameters)
+    {
+        System.Collections.Generic.IEnumerable<DbParameter> aNewProjection =
+            parameters.Select((things) =>
+           {
+               return _parentSqlConn.databaseType == enDatabaseType.SQLServer
+                   ? new SqlParameter(things.paramName, things.paramValue)
+                   : _parentSqlConn.databaseType == enDatabaseType.SqLite
+                   ? new Microsoft.Data.Sqlite.SqliteParameter(things.paramName, things.paramValue)
+                   : (DbParameter)null;
+           });
+
+        _cmdOleDBcommand.Parameters.AddRange(aNewProjection.ToArray());
+        return this;
+    }
+
+    public sqlCommandKu addParamWithValue(string instrParamName, object inoValue, Action<DbParameter> SqlParamCreatedCallBack)
+    {
+        DbParameter odprmReturnValue = null;
+        if (_parentSqlConn.databaseType == enDatabaseType.SQLServer)
         {
-            return addParamWithValue(true, instrParamName, inoValue);
+            odprmReturnValue = new Microsoft.Data.SqlClient.SqlParameter(instrParamName, inoValue);
         }
 
-        public sqlCommandKu addParamWithValue(bool trueToAdd, string instrParamName, object inoValue)
+        if (_parentSqlConn.databaseType == enDatabaseType.SqLite)
         {
-            return trueToAdd ? addParamWithValue(instrParamName, inoValue, null) : this;
+            odprmReturnValue = new Microsoft.Data.Sqlite.SqliteParameter(instrParamName, inoValue);
         }
 
-        public T executeScalar<T>()
+        this.addParams(odprmReturnValue);
+        SqlParamCreatedCallBack?.Invoke(odprmReturnValue);
+        return this;
+    }
+
+    public sqlCommandKu addParamWithValue(string instrParamName, object inoValue)
+    {
+        return addParamWithValue(true, instrParamName, inoValue);
+    }
+
+    public sqlCommandKu addParamWithValue(bool trueToAdd, string instrParamName, object inoValue)
+    {
+        return trueToAdd ? addParamWithValue(instrParamName, inoValue, null) : this;
+    }
+
+    public T executeScalar<T>()
+    {
+        doOpen();
+        object aObject = _cmdOleDBcommand.ExecuteScalar();
+        doClose();
+        return (T)aObject;
+    }
+
+    public void executeNonQuery(bool inblnAutoClose)
+    {
+        doOpen();
+        _ = _cmdOleDBcommand.ExecuteNonQuery();
+
+        if (inblnAutoClose)
         {
-            doOpen();
-            object aObject = _cmdOleDBcommand.ExecuteScalar();
             doClose();
-            return (T)aObject;
         }
+    }
 
-        public void executeNonQuery(bool inblnAutoClose)
-        {
-            doOpen();
-            _ = _cmdOleDBcommand.ExecuteNonQuery();
+    public void executeNonQuery()
+    {
+        executeNonQuery(true);
+    }
 
-            if (inblnAutoClose)
-            {
-                doClose();
-            }
-        }
+    private void clearLastCommand()
+    {
+        _cmdOleDBcommand?.Dispose();
+    }
 
-        public void executeNonQuery()
-        {
-            executeNonQuery(true);
-        }
-
-        private void clearLastCommand()
-        {
-            _cmdOleDBcommand?.Dispose();
-        }
-
-        public void Dispose()
-        {
-            //throw new Exception("The method or operation is not implemented.");
-            // lakukan bersih bersih disini
-            clearLastCommand();
-        }
+    public void Dispose()
+    {
+        //throw new Exception("The method or operation is not implemented.");
+        // lakukan bersih bersih disini
+        clearLastCommand();
     }
 }
--- sqlConnectionKu.cs
+++ sqlConnectionKu.cs
@@ -3,6 +3,8 @@ using ImpromptuInterface;
 using jomiunsExtensions;
 using Microsoft.Data.SqlClient;
 using Microsoft.Data.Sqlite;
+using Microsoft.Identity.Client;
+
 #endif
 using netCore_sqlConnectionKu;
 using System;
@@ -11,193 +13,217 @@ using System.Data;
 using System.Data.Common;
 using System.Linq;
 
-namespace JomiunsCom
+namespace JomiunsCom;
+
+public interface iSqlConnectionKu
 {
-    public partial class sqlConnectionKu : IDisposable
+    sqlCommandKu getSP(string instrNamaSP);
+#if NET6_0_OR_GREATER
+    List<T> getAllFromTable<T>(string tableName, object criteriaAND, bool IsWithNolock, params string[] columnNameToSelect);
+#endif
+}
+
+public class factory()
+{
+    public static iSqlConnectionKu createSQLserver(string instrConnectionString)
     {
-        private sqlCommandKu _cmdLastCommand;
-        public enDatabaseType databaseType { get; private set; }
+        var aTemp = new sqlConnectionKu
+        {
+            theSQLconn = new Microsoft.Data.SqlClient.SqlConnection(instrConnectionString),
+            databaseType = enDatabaseType.SQLServer
+        };
+        return aTemp;
+    }
 
-        internal DbConnection theSQLconn { get; set; }
+    public static sqlConnectionKu create(sqlConnectionKuInfo incInfo)
+    {
+        string strConnectionString = $"data source={incInfo.SQLServer}; uid={incInfo.UserName}; password={incInfo.Password}; initial catalog={incInfo.InitialCatalog}";
+        var sqlconnReturnValue = new sqlConnectionKu
+        {
+            SQLconnInfo = incInfo,
+            theSQLconn = new Microsoft.Data.SqlClient.SqlConnection(strConnectionString),
+            databaseType = enDatabaseType.SQLServer
+        };
+        return sqlconnReturnValue;
+    }
 
-        public sqlConnectionKuInfo SQLconnInfo { get; set; }
+    public static sqlConnectionKu create(string instrServerDanInstance, string instrUsername, string instrPassword, string instrInitialCatalog)
+    {
+        var aInfo = new sqlConnectionKuInfo()
+        {
+            SQLServer = instrServerDanInstance,
+            UserName = instrUsername,
+            Password = instrPassword,
+            InitialCatalog = instrInitialCatalog
+        };
+
+        var objectku = factory.create(aInfo);
+        objectku.databaseType = enDatabaseType.SQLServer;
+        return objectku;
+    }
+#if NET6_0_OR_GREATER
+    public static sqlConnectionKu createSQLlite(string instrSqLiteDBpath)
+    {
+        var aReturnValue = new sqlConnectionKu();
+
+        var connectionStringBuilder = new SqliteConnectionStringBuilder()
+        {
+            DataSource = instrSqLiteDBpath
+        };
+        aReturnValue.theSQLconn = new SqliteConnection(connectionStringBuilder.ConnectionString);
+        aReturnValue.databaseType = enDatabaseType.SqLite;
+        return aReturnValue;
+    }
+#endif
+}
+
+public class sqlConnectionKu : IDisposable, iSqlConnectionKu
+{
+    private sqlCommandKu _cmdLastCommand;
+    public enDatabaseType databaseType { get; internal set; }
+
+    internal DbConnection theSQLconn { get; set; }
+
+    public sqlConnectionKuInfo SQLconnInfo { get; set; }
 
 #if NET5_0_OR_GREATER
-        public object saveDataToTable(string tableName, object toSave, bool getIdentity)
+    public object saveDataToTable(string tableName, object toSave, bool getIdentity)
+    {
+        var retVal = null as object;
+        var theType = toSave.GetType();
+        var properties = theType.GetProperties();
+
+        var yeah = properties.Select(oo => new
         {
-            var retVal = null as object;
-            var theType = toSave.GetType();
-            var properties = theType.GetProperties();
+            columnName = oo.Name
+                           .Replace("open", "[open]", StringComparison.CurrentCultureIgnoreCase)
+                           .Replace("close", "[close]", StringComparison.CurrentCultureIgnoreCase),
+            parameter = this.databaseType == enDatabaseType.SQLServer ?
+                        (DbParameter)new SqlParameter($"@{oo.Name}", oo.GetValue(toSave, null) ?? DBNull.Value) :
+                        (DbParameter)new SqliteParameter($"@{oo.Name}", oo.GetValue(toSave, null) ?? DBNull.Value)
+        });
 
-            var yeah = properties.Select(oo => new
-            {
-                columnName = oo.Name
-                               .Replace("open", "[open]", StringComparison.CurrentCultureIgnoreCase)
-                               .Replace("close", "[close]", StringComparison.CurrentCultureIgnoreCase),
-                parameter = this.databaseType == enDatabaseType.SQLServer ?
-                            (DbParameter)new SqlParameter($"@{oo.Name}", oo.GetValue(toSave, null) ?? DBNull.Value) :
-                            (DbParameter)new SqliteParameter($"@{oo.Name}", oo.GetValue(toSave, null) ?? DBNull.Value)
-            });
+        var columnName = string.Join(",", yeah.Select(oo => oo.columnName).ToArray());
+        var paramName = string.Join(",", yeah.Select(oo => oo.parameter.ParameterName).ToArray());
+        var listParam = yeah.Select(oo => oo.parameter).ToList();
 
-            var columnName = string.Join(",", yeah.Select(oo => oo.columnName).ToArray());
-            var paramName = string.Join(",", yeah.Select(oo => oo.parameter.ParameterName).ToArray());
-            var listParam = yeah.Select(oo => oo.parameter).ToList();
-
-            var SqlCommand = $@"
+        var SqlCommand = $@"
 insert into dbo.{tableName} ({columnName})
 select {paramName}";
 
-            if (getIdentity)
-            {
-                var temp = new[] { SqlCommand, "select @@IDENTITY" };
-                SqlCommand = string.Join(";" + Environment.NewLine, temp);
-            }
-
-            var dsOut = this.getSP(SqlCommand)
-                            .setCommandTypeAsText()
-                            .addParams(listParam.ToArray())
-                            .getDataSet();
-
-            if (getIdentity)
-            {
-                var dtTable = dsOut.Tables[0];
-                var drRow = dtTable.Rows[0];
-                retVal = drRow[0];
-            }
-
-            return retVal;
+        if (getIdentity)
+        {
+            var temp = new[] { SqlCommand, "select @@IDENTITY" };
+            SqlCommand = string.Join(";" + Environment.NewLine, temp);
         }
+
+        var dsOut = ((iSqlConnectionKu)this).getSP(SqlCommand)
+                        .setCommandTypeAsText()
+                        .addParams(listParam.ToArray())
+                        .getDataSet();
+
+        if (getIdentity)
+        {
+            var dtTable = dsOut.Tables[0];
+            var drRow = dtTable.Rows[0];
+            retVal = drRow[0];
+        }
+
+        return retVal;
+    }
 #endif
-        public static sqlConnectionKu createSQLserver(string instrConnectionString)
-        {
-            var aTemp = new sqlConnectionKu
-            {
-                theSQLconn = new Microsoft.Data.SqlClient.SqlConnection(instrConnectionString),
-                databaseType = enDatabaseType.SQLServer
-            };
-            return aTemp;
-        }
-        public static sqlConnectionKu create(sqlConnectionKuInfo incInfo)
-        {
-            string strConnectionString = $"data source={incInfo.SQLServer}; uid={incInfo.UserName}; password={incInfo.Password}; initial catalog={incInfo.InitialCatalog}";
-            var sqlconnReturnValue = new sqlConnectionKu
-            {
-                SQLconnInfo = incInfo,
-                theSQLconn = new Microsoft.Data.SqlClient.SqlConnection(strConnectionString),
-                databaseType = enDatabaseType.SQLServer
-            };
-            return sqlconnReturnValue;
-        }
 
-        public static sqlConnectionKu create(string instrServerDanInstance, string instrUsername, string instrPassword, string instrInitialCatalog)
+    public DataSet getDataSet(System.Reflection.MethodInfo inMethodInfo, ref object[] inListValues)
+    {
+        var strMethodName = inMethodInfo.Name;
+        var intIndex = -1;
+        var aDict = new Dictionary<int, DbParameter>();
+        DataSet dsResult = null;
+        strMethodName = strMethodName.Replace("__", ".");
+        var aCommand = (this as iSqlConnectionKu).getSP(strMethodName);
+
+        foreach (var aParameterInfo in inMethodInfo.GetParameters())
         {
-            var aInfo = new sqlConnectionKuInfo()
-            {
-                SQLServer = instrServerDanInstance,
-                UserName = instrUsername,
-                Password = instrPassword,
-                InitialCatalog = instrInitialCatalog
-            };
-
-            sqlConnectionKu objectku = sqlConnectionKu.create(aInfo);
-            objectku.databaseType = enDatabaseType.SQLServer;
-            return objectku;
-        }
-
-        public sqlCommandKu getSP(string instrNamaSP)
-        {
-            _cmdLastCommand?.Dispose();
-            _cmdLastCommand = null;
-            _cmdLastCommand = new sqlCommandKu(instrNamaSP, this);
-            return _cmdLastCommand;
-        }
-
-        public DataSet getDataSet(System.Reflection.MethodInfo inMethodInfo, ref object[] inListValues)
-        {
-            var strMethodName = inMethodInfo.Name;
-            var intIndex = -1;
-            var aDict = new Dictionary<int, DbParameter>();
-            DataSet dsResult = null;
-            strMethodName = strMethodName.Replace("__", ".");
-            var aCommand = getSP(strMethodName);
-
-            foreach (var aParameterInfo in inMethodInfo.GetParameters())
-            {
-                intIndex++;
-                _ = aCommand.addParamWithValue(
-                    instrParamName: $"@{aParameterInfo.Name}",
-                    inoValue: inListValues[intIndex],
-                    SqlParamCreatedCallBack: (agas) =>
+            intIndex++;
+            _ = aCommand.addParamWithValue(
+                instrParamName: $"@{aParameterInfo.Name}",
+                inoValue: inListValues[intIndex],
+                SqlParamCreatedCallBack: (agas) =>
+                {
+                    if (aParameterInfo.ParameterType.IsByRef)
                     {
-                        if (aParameterInfo.ParameterType.IsByRef)
-                        {
-                            agas.Direction = ParameterDirection.InputOutput;
-                            agas.Size = -1;
-                        }
-                        aDict.Add(intIndex, agas);
-                    });
-            }
-
-            dsResult = aCommand.getDataSet();
-            foreach (KeyValuePair<int, DbParameter> kvp in aDict)
-            {
-                inListValues[kvp.Key] = kvp.Value.Value;
-            }
-            return dsResult;
+                        agas.Direction = ParameterDirection.InputOutput;
+                        agas.Size = -1;
+                    }
+                    aDict.Add(intIndex, agas);
+                });
         }
 
+        dsResult = aCommand.getDataSet();
+        foreach (KeyValuePair<int, DbParameter> kvp in aDict)
+        {
+            inListValues[kvp.Key] = kvp.Value.Value;
+        }
+        return dsResult;
+    }
+
+    public sqlConnectionKuInfo currentSQLinfo()
+    {
+        return SQLconnInfo;
+    }
+
+    public void Dispose()
+    {
+        _cmdLastCommand?.Dispose();
+
+        if (theSQLconn != null)
+        {
+            if (theSQLconn.State == ConnectionState.Open)
+            {
+                theSQLconn.Close();
+            }
+
+            theSQLconn.Dispose();
+        }
+    }
+
+    sqlCommandKu iSqlConnectionKu.getSP(string instrNamaSP)
+    {
+        _cmdLastCommand?.Dispose();
+        _cmdLastCommand = null;
+        _cmdLastCommand = new sqlCommandKu(instrNamaSP, this);
+        return _cmdLastCommand;
+    }
 
 #if NET6_0_OR_GREATER
-        public List<T> getAllFromTable<T>(string tableName, object criteriaAND, bool IsWithNolock, params string[] columnNameToSelect)
-        {
-            var querySelectPart01ResolverInstance = new querySelectPart01Resolver(Impromptu.ActLike<iSomething>(new { tableName, IsWithNolock, columnNameToSelect }));
-            var part01 = querySelectPart01ResolverInstance.solvePart01();
-            var aList = new List<string> { part01 };
-            var part02resolver = new selectQueryPart02Resolver(Impromptu.ActLike<iSelectQueryPart02>(new { criteriaAND, databaseType }));
-            var (whereCriteriaResult, listParamResult) = part02resolver.solvePart02();
-
-            if (whereCriteriaResult.isNotNullOrEmpty())
-                aList.Add(whereCriteriaResult);
-
-            var sqlCommand = string.Join(" where ", aList);
-
-            var dsOut = this.getSP(sqlCommand)
-                .setCommandTypeAsText()
-                .addParams(listParamResult)
-                .getDataSet();
-
-            var aTable = dsOut.Tables[0];
-            var returnValue = aTable.toListObject<T>();
-            return returnValue;
-        }
-#endif
-
-        public sqlConnectionKuInfo currentSQLinfo()
-        {
-            return SQLconnInfo;
-        }
-
-        public void Dispose()
-        {
-            _cmdLastCommand?.Dispose();
-
-            if (theSQLconn != null)
-            {
-                if (theSQLconn.State == ConnectionState.Open)
-                {
-                    theSQLconn.Close();
-                }
-
-                theSQLconn.Dispose();
-            }
-        }
-
-    }
-
-    public enum enDatabaseType
+    List<T> iSqlConnectionKu.getAllFromTable<T>(string tableName, object criteriaAND, bool IsWithNolock, params string[] columnNameToSelect)
     {
-        SQLServer = 1,
-        SqLite = 2
-    }
+        var querySelectPart01ResolverInstance = new querySelectPart01Resolver(Impromptu.ActLike<iSomething>(new { tableName, IsWithNolock, columnNameToSelect }));
+        var part01 = querySelectPart01ResolverInstance.solvePart01();
+        var aList = new List<string> { part01 };
+        var part02resolver = new selectQueryPart02Resolver(Impromptu.ActLike<iSelectQueryPart02>(new { criteriaAND, databaseType }));
+        var (whereCriteriaResult, listParamResult) = part02resolver.solvePart02();
 
+        if (whereCriteriaResult.isNotNullOrEmpty())
+            aList.Add(whereCriteriaResult);
+
+        var sqlCommand = string.Join(" where ", aList);
+
+        var dsOut = (this as iSqlConnectionKu).getSP(sqlCommand)
+            .setCommandTypeAsText()
+            .addParams(listParamResult)
+            .getDataSet();
+
+        var aTable = dsOut.Tables[0];
+        var returnValue = aTable.toListObject<T>();
+        return returnValue;
+    }
+    
+#endif
+}
+
+public enum enDatabaseType
+{
+    SQLServer = 1,
+    SqLite = 2
 }
